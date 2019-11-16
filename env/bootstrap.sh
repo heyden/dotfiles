0ff6f88b67f6102ae5b2a346d7807711fa52c231
Kris <kris@localhost.localdomain>
2019-11-16 12:43:10 -0600

    add bootstrap an install

---
+++ env/bootstrap.sh
@@ -0,0 +1,16 @@
+
+if [ -f "$HOME/dotfiles/env/aliases" ]; then
+    source "$HOME/dotfiles/env/aliases"
+fi
+
+if [ -f "$HOME/dotfiles/env/functions" ]; then
+    source "$HOME/dotfiles/env/functions"
+fi
+
+if [ -f "$HOME/dotfiles/env/vars" ]; then
+    source "$HOME/dotfiles/env/vars"
+fi
+
+if [ -f "$HOME/dotfiles/env/secrets" ]; then
+    source "$HOME/dotfiles/env/secrets"
+fi
---
+++ install.sh
@@ -0,0 +1,3 @@
+
+
+echo '[ -s "$HOME/dotfiles/env/bootstrap.sh" ] && \. $HOME/dotfiles/env.boostrap.sh' >> $HOME/.zshrc
